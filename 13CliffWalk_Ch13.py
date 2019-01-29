c5c37778836e2573df63b372c498d2cbeb8cf98a
shawn <s1798190@ed.ac.uk>
2019-01-29 13:44:50 +0000

    working on debugging REINFORCE algorithm which can run now however, modle never converges
    Todo:
     - fix bug in 3CarRental_Ch5
     - REINFOCE w/o baseline
     - Chapter 13

--- 13CliffWalk_Ch13.py
+++ 13CliffWalk_Ch13.py
@@ -4,37 +4,22 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
-import gym
-from tensorboardX import SummaryWriter
 from collections import namedtuple, deque
 
 # configurations
-env_name = 'CartPole-v1'
-gamma = 0.99
-lr = 0.001
-goal_score = 200
+gamma = 1.00
+lr = 2e-12
+best_score = -14
 log_interval = 10
+test_interval = 20
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 Transition = namedtuple('Transition', ('state', 'next_state', 'action', 'reward', 'mask'))
-
-
-class ReplayPool(object):
-    def __init__(self):
-        self.memory = deque()
-
-    def push(self, state, next_state, action, reward, mask):
-        self.memory.append(Transition(state, next_state, action, reward, mask))
-
-    def sample(self):
-        memory = self.memory
-        return Transition(*zip(*memory))
-
-    def __len__(self):
-        return len(self.memory)
+torch.manual_seed(2333)
+np.random.seed(1234)
 
 
 class CliffWalking(object):
-    def __init__(self):
+    def __init__(self, step_limit=50):
         self.shape = (4, 12)
 
         # always start from the left-dow corner
@@ -59,6 +44,13 @@ class CliffWalking(object):
         }
 
         self.transmit_tensor = self._build_transmit_tensor_()
+
+        self.num_actions = len(self.actions)
+        self.state_dim = 2
+
+        # this is a rule added by Shawn
+        self.steps = 0
+        self.step_limit = step_limit
 
     def _build_transmit_tensor_(self):
         trans_matrix = [[[] for _ in range(self.shape[1])] for __ in range(self.shape[0])]
@@ -92,9 +84,15 @@ class CliffWalking(object):
         return new_pos, reward, terminate
 
     def take_action(self, action):
-        new_pos, reward, terminate = self.transmit_tensor[self.pos[0]][self.pos[1]][self.actions[action]]
+        if self.steps > self.step_limit:
+            return self.pos, -100, True
+        if isinstance(action, str):
+            new_pos, reward, terminate = self.transmit_tensor[self.pos[0]][self.pos[1]][self.actions[action]]
+        else:
+            new_pos, reward, terminate = self.transmit_tensor[self.pos[0]][self.pos[1]][action]
         self.pos[0] = new_pos[0]
         self.pos[1] = new_pos[1]
+        self.steps += 1
         return new_pos, reward, terminate
 
     def show_pos(self):
@@ -104,18 +102,19 @@ class CliffWalking(object):
 
     def reset(self):
         self.pos = np.asarray([self.shape[0] - 1, 0])
+        self.steps = 0
         return self.pos
 
 
 class REINFORCE(nn.Module):
-    def __init__(self, input_dim, output_dim):
+    def __init__(self, input_dim, output_dim, hidden_dim=48):
         super(REINFORCE, self).__init__()
 
         self.input_dim = input_dim
         self.output_dim = output_dim
 
-        self.fc1 = nn.Linear(input_dim, 128)
-        self.fc2 = nn.Linear(128, output_dim)
+        self.fc1 = nn.Linear(input_dim, hidden_dim)
+        self.fc2 = nn.Linear(hidden_dim, output_dim)
 
         for m in self.modules():
             # in case add more modules later
@@ -128,7 +127,7 @@ class REINFORCE(nn.Module):
         return policy
 
     @classmethod
-    def train_model(cls, model, transitions, optimizer):
+    def train_model(cls, model, transitions, optimizer, gamma=1.0):
         states, actions, rewards, masks = transitions.state, transitions.action, transitions.reward, transitions.mask
 
         states = torch.stack(states)
@@ -164,79 +163,97 @@ class REINFORCE(nn.Module):
         return action
 
 
+class ReplayPool(object):
+    def __init__(self):
+        self.memory = deque()
+
+    def push(self, state, next_state, action, reward, mask):
+        self.memory.append(Transition(state, next_state, action, reward, mask))
+
+    def sample(self):
+        memory = self.memory
+        return Transition(*zip(*memory))
+
+    def __len__(self):
+        return len(self.memory)
+
+
 def test_cliff_warlking_by_hand(cw):
     _, reward, terminate = cw.transmit_tensor[3][0][0]
     cw.show_pos()
+    score = 0
     while not terminate:
         action = input('input your actoin (U/D/L/R):')
         new_pos, reward, terminate = cw.take_action(action)
-        print(new_pos, reward, terminate)
+        score += reward
+        print(new_pos, reward, terminate, score)
         cw.show_pos()
 
 
-def tmp_use_gym():
-    env = gym.make(env_name)
-    env.seed(500)
-    torch.manual_seed(500)
+def train_REINFORCE():
+    cw = CliffWalking()
+    input_dim = cw.state_dim
+    output_dim = cw.num_actions
+    print('state size:', input_dim)
+    print('action size:', output_dim)
 
-    num_inputs = env.observation_space.shape[0]
-    num_actions = env.action_space.n
-    print('state size:', num_inputs)
-    print('action size:', num_actions)
+    model = REINFORCE(input_dim, output_dim)
 
-    net = REINFORCE(num_inputs, num_actions)
+    optimizer = optim.RMSprop(model.parameters(), lr=lr)
 
-    optimizer = optim.Adam(net.parameters(), lr=lr)
-    writer = SummaryWriter('logs')
-
-    net.to(device)
-    net.train()
-    running_score = 0
+    model.to(device)
+    model.train()
     steps = 0
-    loss = 0
 
     for e in range(3000):
-        done = False
-        replay_pool = ReplayPool()
-
         score = 0
-        state = env.reset()
-        state = torch.Tensor(state).to(device)
-        state = state.unsqueeze(0)
+        state = cw.reset()
+        state = torch.Tensor(state).to(device).unsqueeze(0)
 
-        while not done:
+        terminate = False
+        replay_pool = ReplayPool()
+        # create an episode
+        while not terminate:
             steps += 1
 
-            action = net.get_action(state)
-            next_state, reward, done, _ = env.step(action)
+            action = model.get_action(state)
+            next_state, reward, terminate = cw.take_action(action)
 
             next_state = torch.Tensor(next_state)
             next_state = next_state.unsqueeze(0)
 
-            mask = 0 if done else 1
-            reward = reward if not done or score == 499 else -1
+            mask = 0 if terminate else 1
+            reward = reward if not terminate or score == -14 else -1
 
-            action_one_hot = torch.zeros(2)
+            action_one_hot = torch.zeros(output_dim)
             action_one_hot[action] = 1
             replay_pool.push(state, next_state, action_one_hot, reward, mask)
 
             score += reward
             state = next_state
 
-        loss = REINFORCE.train_model(net, replay_pool.sample(), optimizer)
+        loss = REINFORCE.train_model(model, replay_pool.sample(), optimizer)
+        print('[loss]episode %d: %.2f' % (e, loss))
 
-        score = score if score == 500.0 else score + 1
-        running_score = 0.99 * running_score + 0.01 * score
-        if e % log_interval == 0:
-            print('{} episode | score: {:.2f}'.format(
-                e, running_score))
-            writer.add_scalar('log/score', float(running_score), e)
-            writer.add_scalar('log/loss', float(loss), e)
-
-        if running_score > goal_score:
-            break
+        if e % test_interval == 0 and (not e == 0):
+            returns = []
+            model.eval()
+            for i in range(100):
+                terminate = False
+                state = cw.reset()
+                state = torch.Tensor(state).to(device)
+                state = state.unsqueeze(0)
+                total_return = 0
+                while not terminate:
+                    action = model.get_action(state)
+                    next_state, reward, terminate = cw.take_action(action)
+                    total_return += reward
+                returns.append(total_return)
+            model.train()
+            print(e, np.mean(np.asarray(returns)))
 
 
 if __name__ == '__main__':
-    cw = CliffWalking()
-    tmp_use_gym()
+    train_REINFORCE()
+    # cw = CliffWalking()
+    # test_cliff_warlking_by_hand(cw)
